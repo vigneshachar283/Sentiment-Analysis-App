0e1336685eb3cd2b4250306133257e1c66bd3021
Vignesh <vignesh24cs@rnsit.ac.in>
2026-07-02 22:51:19 +0530

    Remove unnecessary newline in app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from textblob import TextBlob
 
 app = Flask(__name__)
-
 @app.route("/", methods=["GET", "POST"])
 def home():
     sentiment = None
